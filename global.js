d8331cf857c27a1e323393e26711a54cfcb67f4a
Lucas Carvalho <lucascarvalholc2001@gmail.com>
2022-03-04 14:38:07 -0300

    modal for new piu

--- global.js
+++ global.js
@@ -1,20 +1,26 @@
 const feed = document.querySelector('.all-posts');
 
+const piuPost = document.querySelector('.piu-open-modal');
+const enviarPiu = document.querySelector('.piu-btn');
+const cancelarPost = document.querySelector('.cancel-btn');
+const janelaModal = document.querySelector('.modal-janela-piu');
+const bodyFeed = document.querySelector('.body-feed');
+console.log(bodyFeed);
+
 async function getData() {
-    
     const rawData = await fetch('https://api.json-generator.com/templates/BQZ3wDrI6ts0/data?access_token=n7lhzp6uj5oi5goj0h2qify7mi2o8wrmebe3n5ad');
     const data = await rawData.json();
     console.log(data);
     
-    data.forEach(user => {
+    data.forEach(usuario => {
         const html = `<div class="post-template">
                     <div class="user-info">
                         <div class="img-user">
-                            <img src="${user.user.photo}" alt="">
+                            <img src="${usuario.user.photo}" alt="">
                         </div>
                         <div class="infos-gerais">
-                            <h3 class="user-name">${user.user.username}</h3>
-                            <p class="user-post-text">${user.text}</p>
+                            <h3 class="user-name">${usuario.user.username}</h3>
+                            <p class="user-post-text">${usuario.text}</p>
                         </div>
                     </div>
                     <div class="post-icons">
@@ -22,9 +28,27 @@ async function getData() {
                         <img src="../assets/heart-post.svg" alt="curtir postagem" class="icon-post">
                         <img src="../assets/more-option.svg" alt="mais opções" class="icon-post">
                     </div>
-                </div>`;
+                </div>`; 
+        
         feed.insertAdjacentHTML('beforeend', html);
+                
     })
+
 }
 
 getData();
+
+piuPost.addEventListener('click', () => {
+    janelaModal.classList.remove('hidden');
+    bodyFeed.classList.add('blur-bg');
+})
+
+cancelarPost.addEventListener('click', () => {
+    janelaModal.classList.add('hidden');
+    bodyFeed.classList.remove('blur-bg');
+})
+
+enviarPiu.addEventListener('click', () => {
+    janelaModal.classList.add('hidden');
+    bodyFeed.classList.remove('blur-bg');
+})
